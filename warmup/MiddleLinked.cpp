42bc5ed0178a6e129508f7f45feab397ad12c310
Norberto01 <norbertoortiz01@gmail.com>
2024-01-20 16:46:36 -0400

    Solving MiddleLinked

---
+++ warmup/MiddleLinked.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ */
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        
+    }
+};
