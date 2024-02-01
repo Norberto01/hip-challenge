8346798b506918e798524eeae3c1c5e32d57b78e
Norberto01 <norbertoortiz01@gmail.com>
2024-01-31 21:31:13 -0400

    Solving FindTargetIdx

---
+++ easy/findTargetIndex.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+class Solution{
+    public:
+        vector<int> targetIndices(vector<int>& nums, int target) {
+            int lenNum = nums.size();
+            int left = 0;
+            int right = lenNum - 1;
+            vector<int> rv = {};
+            sort(nums.begin(), nums.end());
+            int count = 0;
+            int mid = left + ((right - left) / 2);
+            cout << "---NM: " << mid;
+            while(left <= right) {
+                cout << endl << "*)C("<< count << ") M:" << mid << " | V(M):" << nums.at(mid) << " | T:" << target << " | L:" << left << " | R:" << right << endl;
+                if (nums.at(mid) == target) {
+                    rv.insert(rv.begin(), mid);
+                    // left = mid + 1;
+                    right = mid - 1;
+                } else if (nums.at(mid) < target) {
+                    left = mid + 1;
+                    // right = mid - 1;
+                }else{
+                    left ++;
+                    right --;
+                }
+                mid = left + ((right - left) / 2);
+                cout << "---NM: " << mid;
+                count ++;
+            }
+            return rv;
+        }
+};
+int main() {
+    // Example usage
+    Solution solution;
+
+    vector<int> edgesB = {100, 1, 100}; // {1, 2}
+    int target = 100;
+    vector<int> rvs = solution.targetIndices(edgesB, target);
+    cout << endl << "RV: ----> {";
+    for (int i: rvs) {
+        cout << i << ", ";
+    }
+    cout << "}";
+    cout << endl << "------------------------------------------------------------------------" << endl;
+    vector<int> edgesA = {1, 2, 5, 2, 3}; // {1, 2}
+    int targetA = 2;
+    vector<int> rvsA = solution.targetIndices(edgesA, targetA);
+    cout << endl << "RV: ----> {";
+    for (int i: rvsA) {
+        cout << i << ", ";
+    }
+    cout << "}";
+}
